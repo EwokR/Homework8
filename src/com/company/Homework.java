933d1d42a0839ea97dade12fa3c2f11f28a3b09a
Anton <imewokrus@gmail.com>
2022-04-20 12:31:01 +0300

    First commit

--- src/com/company/Homework.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Homework {
-
-    public static void main(String[] args) {
-	// write your code here
-
-    }
-}
---
+++ src/com/company/Main.java
@@ -0,0 +1,72 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        doExercise1(2022);
+        String osName = "iOS";
+        doExercise2(osName,2018);
+        doExercise3 (95);
+        int[] massive = {3, 2, 1, 6, 5};
+    }
+
+    public static void doExercise1(int year) {
+        // Exercise 1
+        if (year % 100 == 0 && year % 400 == 0)
+            System.out.println(year + " this year is intercalary year.");
+        else if (year % 4 == 0 && year % 100 > 0)
+            System.out.println(year + " this year is interalary year.");
+        else if (year % 100 == 0)
+            System.out.println(year + " this year is not interalary year.");
+        else
+            System.out.println(year + " this year is not interalary year.");
+    }
+
+    // Exercise 2
+    public static int doExercise2(String name, int phoneYear) {
+        public static int getClientOS(String name){
+            if (name.equals("iOS")) {
+                return 0;
+            } else {
+                return 1;
+            }
+            int currentYear = 2022;
+            int clientOS = getClientOS(osName);
+            if (phoneYear < currentYear && clientOS == 0)
+                System.out.println("Please download lite IOS app for your phone. Use this link ...");
+            else if (clientOS == 1 && phoneYear < currentYear)
+                System.out.println("Please download lite Android app for your phone. Use this link ...");
+            else if (phoneYear == currentYear && clientOS == 0)
+                System.out.println("Download IOS app from this link.");
+            else if (phoneYear == currentYear && clientOS == 1)
+                System.out.println("Download Android app from this link.");
+        }
+    }
+    // Exercise 3
+    public static void doExercise3(int deliveryDistance) {
+        if (deliveryDistance <= 20)
+            System.out.println("Cart arrive next day.");
+        else if (deliveryDistance >= 20 && deliveryDistance <= 60)
+            System.out.println("Cart arrive after two days.");
+        else if (deliveryDistance >= 60 && deliveryDistance <= 100)
+            System.out.println("Cart arrive after three days.");
+        else
+            System.out.println("Cart arrive on a work week.");
+    }
+    // Exercise 4
+
+    // Exercise 5
+    public static void doExercise5(int[] massive) {
+        System.out.println();
+        for (int i = massive.length -1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(massive[i]);
+            } else {
+                System.out.print(massive[i] + ", ");
+            }
+        }
+    }
+
+
+}
+
